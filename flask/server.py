b6cbc162826594bd4768f71bf2c278345ba064d7
vagrant <vagrant@vagrant.vm>
2017-04-24 03:18:27 +0000

    assessment complete

---
+++ flask/server.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+app.jinja_env.undefined = StrictUndefined
+app.jinja_env.auto_reload = True
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+JOBS = [
+	"Software Engineer", "Product Manager", "QA Engineer", "Data Scientist", "Engineering Evangelist",
+]
+
+# YOUR ROUTES GO HERE
+@app.route('/')
+def start_here():
+	"""Displays Homepage."""
+	return render_template("index.html")
+
+@app.route('/application-form')
+def application():
+	"""Job selection page for user."""
+	return render_template("application-form2.html", jobs = JOBS)
+
+@app.route('/application-success', methods=["POST"])
+def succes():
+	firstname = request.form.get("firstname")
+	lastname = request.form.get("lastname")
+	salary = int(request.form.get("salary"))
+	position = request.form.get("position")
+
+	return render_template("application-response.html", firstname = firstname, 
+														lastname = lastname,
+														salary = salary,
+														position = position)
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
